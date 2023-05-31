c26fb6165ed3f4a03b5107b954111369c61ff99a
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-05-31 05:01:43 -0700

    added deployment configuratio

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x719716219B2e905D0F13434832d378f18C69EC05";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
